2fcfa825984b53b365d813042310df6e1f292326
khang <aslka126@gmail.com>
2023-05-08 00:43:50 -0700

    Add register function and password checker

--- sessions.js
+++ sessions.js
@@ -9,11 +9,17 @@ const sessions = require('client-sessions');
 // Import UUID
 const { v4: uuidv4 } = require('uuid');
 
-// The body parser
+// Import body parser
 const bodyParser = require("body-parser");
 
-// The mysql library
+// Import mysql library
 const mysql = require('mysql');
+
+// Import bcrypt library
+const bcrypt = require('bcrypt');
+
+// Import check-password-strength library
+const { passwordStrength } = require('check-password-strength');
 
 // Instantiate an express app
 const app = express();
@@ -28,7 +34,10 @@ const mysqlConn = mysql.createConnection({
 	password: "apppass",
 	multipleStatements: true
 });
-mysqlConn.connect();
+mysqlConn.connect((err) => {
+	if (err) throw err;
+	console.log("Connected to MariaDB!");
+});
 
 
 // Needed to parse the request body
@@ -48,9 +57,12 @@ app.use(sessions({
 	activeDuration: 5 * 60 * 1000,
 })); 
 
-// The default page
-// @param req - the request
-// @param res - the response
+/**
+ * The default page 
+ * @param req - the request 
+ * @param res - the response
+ */
+
 app.get("/", function(req, res){
 	
 	// Is this user logged in?
@@ -62,20 +74,22 @@ app.get("/", function(req, res){
 	else
 	{
 		// No!
-		res.render('loginpage');
+		res.render('loginpage.ejs', { message: null });
 	}
 
 });
 
-// The login page
-// @param req - the request
-// @param res - the response
+/**
+ * The dashboard page
+ * @param req - the request
+ * @param res - the response
+ */
 app.get('/dashboard', function(req, res){
 	
 	// Is this user logged in? Then show the dashboard
 	if(req.session.username)
 	{
-		res.render('dashboard', {username: req.session.username});
+		res.render('dashboard.ejs', {username: req.session.username});
 	}
 	//Not logged in! Redirect to the mainpage
 	else
@@ -85,103 +99,146 @@ app.get('/dashboard', function(req, res){
 
 });
 
-// The login script
-// @param req - the request
-// @param res - the response
+/**
+ * The login script
+ * @param req - the request
+ * @param res - the response
+ */
 app.post('/login', function(req, res){
 	
-	// Get the username and password data from the form
-	let userName = req.body.username;
-	let password = req.body.password;
+	// Get the username data from the form
+	const userName = req.body.username;
+
+	// Get the password data from the form and hash with bcrypt
+	const password = req.body.password;
 	
 	
 	// Construct the query
-	let query = "USE users;SELECT username,password FROM appusers WHERE username=? AND password=?"; 
-	console.log(query);
-			
-				
+	let query = "USE users;SELECT username,password FROM appusers WHERE username=?"; 
 	// Query the DB for the user
-	mysqlConn.query(query, [userName, password], function(err, qResult){
-					
+	mysqlConn.query(query, [userName], function(err, qResult){				
 		if(err) throw err;
+		
+		if (qResult[1].length <= 0) {
+			res.render('loginpage.ejs', { message: 'Username does not exist!' });
+		}
+		else {
+			// Check password
+			// Get the hashed password from the DB
+			const hashedPassDB = qResult[1][0]['password'];
+	
+			// Hash and compare the password with the hashed password in DB
+			bcrypt.compare(password, hashedPassDB, (err, result) => {
+				if (err) throw err;
+				
+				// Passwords matched
+				if (result) {
+					// Set the session variable 
+					req.session.username = userName;
+					req.session.id = uuidv4();
 					
-		if(qResult.length > 0) {
-			req.session.username = userName;
-			req.session.id = uuidv4();
-
-			let updateSessionQuery = "UPDATE appusers SET session='" + req.session.id + "' WHERE username='" + req.session.username + "'";
-			console.log(updateSessionQuery);
-			mysqlConn.query(updateSessionQuery, function(err) {
-                if (err) throw err;
-            });
-
-			res.redirect('/dashboard');
-
-		} else {
-			res.send("<b>Wrong</b>");
+					// Update the session ID in the database
+					let updateSessionQuery = "UPDATE appusers SET session='" + req.session.id + "' WHERE username='" + req.session.username + "'";
+					mysqlConn.query(updateSessionQuery, function(err) {
+						if (err) throw err;
+					});
+					res.redirect('/dashboard');
+				}
+				// Passwords not matched
+				else {
+					res.render('loginpage.ejs', { message: 'Incorrect password!' });
+				}
+			});
 		}
 		
-		// // Does the password match?
-		// let match = false;
-			
-		// // Go through the results of the second query
-		// for(let account of qResult[1])
-		// {
-			
-		// 	if(account['username'] == userName && account['password'] == password)
-		// 	{
-		// 		console.log("Match!");
-				
-		// 		// We have a match!
-		// 		match = true;
-				
-		// 		//break;
-				
-		// 	}
-		// };
-
-		// // Login succeeded! Set the session variable and send the user
-		// // to the dashboard
-		// if(match)
-		// {
-		// 	req.session.username = userName;
-		// 	res.redirect('/dashboard');
-		// }
-		// else
-		// {
-		// 	// If no matches have been found, we are done
-		// 	res.send("<b>Wrong</b>");				
-		// }
+		
 	});
 	
-	
-	
-	/**
-	if(correctPass && correctPass === password)
-	{
-		// Set the session
-		req.session.username = userName;
-
-		res.redirect('/dashboard');
-	}
-	else
-	{
-		res.send("Wrong!");
-	}
-	
-	res.send("Wrong");
-
-**/
 });
 
-// The logout function
-// @param req - the request
-// @param res - the response
+/**
+ * The register page
+ * @param req - the request
+ * @param res - the response
+ */
+app.get('/register', function(req, res){
+	res.render('register.ejs', { message: null });
+});
+
+/**
+ * The register script
+ * @param req - the request
+ * @param res - the response
+ */
+app.post('/register', function(req, res){
+	const { username, new_password, confirm_password, info } = req.body;
+
+	// Check if username has been taken
+	const queryCheckUsername = "USE users;SELECT COUNT(*) AS count FROM appusers WHERE username=?";
+	mysqlConn.query(queryCheckUsername, [username], (err, qResult) => {
+		if (err) throw err;
+		if (qResult[1][0]['count'] !== 0){
+			return res.render('register.ejs', { message: "Your username has been taken!" });
+		}
+		else {
+			// Check password strength
+			const passwordStrengthResult = passwordStrength(new_password);
+			if (passwordStrengthResult.value !== "Medium") {
+				res.render('register.ejs', 
+					{ message: 'Your password is not strong enough'}
+				);
+			}
+			else {
+				// Check if password confirmation is correct
+				if (new_password !== confirm_password) {
+					res.render('register.ejs', 
+						{ message: "New password and confirm password do not match."});
+				}
+				else {
+					// Store the new credentials into MariaDB
+					
+					// Hash the new password
+					bcrypt.hash(new_password, 10, (err, hashedPassword) => {
+						if (err) throw err;
+						// Create a query to store the credentials
+						const queryCreateNewAcct = "INSERT INTO appusers VALUES(?,?,?,?)";
+						mysqlConn.query(queryCreateNewAcct, [username, hashedPassword, info, null], (err) => {
+							if (err) {
+								throw err;
+							}
+							else {
+								res.render("loginpage.ejs", { message: "New account has been created" });
+								//"Asd1234!"
+							}
+						})
+					});
+				}
+			}
+		}
+	});
+
+
+
+
+
+
+})
+
+/**
+ * The logout function
+ * @param req - the request
+ * @param res - the response
+ */
 app.get('/logout', function(req, res){
+	
+	// Disconnect MariaDB
+	mysqlConn.end((err) => {
+		if (err) throw err;
+		console.log("Disconnected from MariaDB!");
+	});
 	
 	// Kill the session
 	req.session.reset();
-	
 	res.redirect('/');
 });
 
